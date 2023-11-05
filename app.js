4fb39315bdc08f8fc0fc0323f7fcf07982f2297d
Sihle <msihlesndlovu97@gmail.com>
2023-11-05 06:09:12 +0200

    save button installed, but need to reset count when saved is clicked

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-document.getElementById("count-el").innerText = 5;
+/* document.getElementById("count-el").innerText = 6; //html changed by the code in JS without touching or clicking anything */
+
+/* const choices = [1, 3, 5, 6, 12, 7];
+const randomIndex = 0 + Math.floor(Math.random() * choices.length);
+newRandomNumber = choices[randomIndex]; */
+let count = 0;
+
+function increment() {
+  /* document.getElementById("count-el").innerText = 6; */
+  count = count + 1;
+  document.getElementById("count-el").innerText = count;
+}
+function save() {
+  document.getElementById("save-el").innerText = count;
+  document.getElementById("count-el").innerText = 0;
+}
